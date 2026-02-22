b8d3c5cd127e3175c762892194033ad2abce5510
shradhey91 <shradheyrastogi91@gmail.com>
2026-02-22 14:16:16 +0530

    made improvements

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,5 +4,6 @@ const authController = require('../controllers/authController');
 
 router.post('/signup', authController.register);
 router.post('/login', authController.login);
+router.get('/experts', authController.getExperts);
 
 module.exports = router;
